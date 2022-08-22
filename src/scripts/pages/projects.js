58a953ba95637a323c0bed84896f597897e3b63b
nneddk <meljandre.mc@gmail.com>
2022-08-22 12:59:04 +0800

    added skeletons for project tabs

--- src/scripts/pages/projects.js
+++ src/scripts/pages/projects.js
@@ -1,5 +1,14 @@
 import '../../styles/pages/projects.css';
 
+let  projectArray = [];
+
+class projectForm{
+    constructor(title, description){
+        this.title = title;
+        this.description = description;
+    }
+}
+
 export default function projects(){
     const projects = document.createElement('div');
     projects.classList.add('projects-div');
@@ -8,6 +17,15 @@ export default function projects(){
     const projectsList = document.createElement('div');
     projectsList.classList.add('projects-list');
     
+    const pinnedProjectsDiv = document.createElement('div');
+    pinnedProjectsDiv.classList.add('pinned-projects');
+
+    const addedProjects = document.createElement('div');
+    addedProjects.classList.add('added-projects');
+
+    projectsList.appendChild(pinnedProjectsDiv);
+    projectsList.appendChild(addedProjects);
+
     const addProject = document.createElement('button');
     addProject.classList.add('add-project-btn')
     addProject.setAttribute('type', 'button');
@@ -17,13 +35,19 @@ export default function projects(){
         newProject();
     }
 
+    
 
     projects.appendChild(addProject);
     projects.appendChild(projectsList);
+
+    let v = 0;
+    projectArray.forEach(element => {
+        addedProjects.appendChild(addProjectTab(element.title, v));
+        v++;
+    });
     return projects;
 
 }
-
 function newProject(){
     const projectWrapper = document.getElementById('wrapper');
     projectWrapper.style.zIndex = '1';
@@ -73,12 +97,14 @@ function newProject(){
     projectEntryAddBtn.setAttribute('type', 'button');
     projectEntryAddBtn.classList.add('project-entry-add-btn');
     projectEntryAddBtn.textContent = 'Add project';
+
     projectEntryAddBtn.onclick = function(){
-        
+        let index =  projectArray.length;
+        const currProjects = document.querySelector('.added-projects');
         const projectTitle = document.querySelector('.project-entry-title').value;
         const projectDesc = document.querySelector('.project-entry-description').value;
         projectArray.push(new projectForm(projectTitle, projectDesc));
-        
+        currProjects.appendChild(addProjectTab(projectTitle, index));     
         console.log(projectArray);
 
         projectWrapper.style.zIndex = '-1';
@@ -93,12 +119,56 @@ function newProject(){
 
     projectWrapper.appendChild(projectEntry);
 }
+let editIndex;
 
-let  projectArray = [];
+function addProjectTab(name, index){
+    const projectTab = document.createElement('div');
+    projectTab.classList.add('project-tab');
 
-class projectForm{
-    constructor(title, description){
-        this.title = title;
-        this.description = description;
+    projectTab.onclick = function(){
+        editIndex = index;
+        console.log(editIndex);
+        viewProject(index);
     }
+    const projectTabTitle = document.createElement('div');
+    projectTabTitle.classList.add('project-tab-title');
+    projectTabTitle.textContent = name;
+
+    projectTab.appendChild(projectTabTitle);
+
+    return projectTab;
 }
+
+
+
+function viewProject(index){
+    const projectWrapper = document.getElementById('wrapper');
+    projectWrapper.style.zIndex = '1';
+
+    const projectEntry = document.createElement('div');
+    projectEntry.classList.add('project-entry-div');
+
+    const viewProjectTitle = document.createElement('div');
+    viewProjectTitle.classList.add('view-project-title');
+    viewProjectTitle.textContent = projectArray[index].title;
+
+    const projectEntryCloseBtn = document.createElement('button');
+    projectEntryCloseBtn.setAttribute('type', 'button');
+    projectEntryCloseBtn.classList.add('project-entry-close-btn');
+    projectEntryCloseBtn.textContent = 'x';
+    projectEntryCloseBtn.onclick = function(){
+        projectWrapper.style.zIndex = '-1';
+    }
+
+    projectEntry.appendChild(viewProjectTitle);
+    projectEntry.appendChild(projectEntryCloseBtn);
+
+
+    while(projectWrapper.hasChildNodes()){
+        projectWrapper.removeChild(projectWrapper.lastChild);
+    }
+
+    projectWrapper.appendChild(projectEntry);
+
+}
+
